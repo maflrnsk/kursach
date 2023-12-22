db07d65d971e2edc1e03569976e72d253225b6eb
maflrnsk <153613775+maflrnsk@users.noreply.github.com>
2023-12-22 17:08:47 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,90 +1,80 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const showMoreButton = document.querySelector(".showmore");
-    if (showMoreButton) {
-      let moreImagesVisible = false;
-  
-      showMoreButton.addEventListener("click", function() {
-        const hiddenImages = document.querySelectorAll(".unshown");
-        if (hiddenImages) {
-          hiddenImages.forEach(function(image) {
-            if (!moreImagesVisible) {
-                image.style.display = "inline";
-            } else {
-                image.style.display = "none";
-            }
-          });
-        }
-    
-        moreImagesVisible = !moreImagesVisible;
-    
-        if (moreImagesVisible) {
-            showMoreButton.textContent = "Скрыть";
+  const showMoreButton = document.querySelector(".showmore");
+  let moreImagesVisible = false;
+
+  showMoreButton.addEventListener("click", function() {
+    const hiddenImages = document.querySelectorAll(".unshown");
+    hiddenImages.forEach(function(image) {
+        if (!moreImagesVisible) {
+            image.style.display = "inline";
         } else {
-            showMoreButton.textContent = "Показать больше";
+            image.style.display = "none";
         }
-      });
+    });
+
+    moreImagesVisible = !moreImagesVisible;
+
+    if (moreImagesVisible) {
+        showMoreButton.textContent = "Скрыть";
+    } else {
+        showMoreButton.textContent = "Показать больше";
     }
   });
-document.addEventListener("DOMContentLoaded", function() {  
-    const dropdown = document.querySelector(".dropdown");
-    const dropdownContent = document.querySelector(".dropdown-content");
-    const dropbtn = document.getElementsByClassName("dropbtn")[0];
-    
-    if (dropdown && dropdownContent && dropbtn) {
-      dropdown.addEventListener("mouseover", function() {
-          if (window.innerWidth > 800) {
-              dropdownContent.style.display = "block";
-          }
-      });
-      
-      dropdown.addEventListener("mouseout", function() {
-          if (window.innerWidth > 800) {
-              dropdownContent.style.display = "none";
-          }
-      });
-    }
+});
+
+const dropdown = document.querySelector(".dropdown");
+const dropdownContent = document.querySelector(".dropdown-content");
+const dropbtn = document.getElementsByClassName("dropbtn")[0]
+document.addEventListener("DOMContentLoaded", function() {
   
-    window.addEventListener("resize", function () {
-      Resized()
-    });
-    
-    function Resized() {
-      if(window.innerWidth < 800) {
-          dropbtn.innerText = "";
+  dropdown.addEventListener("mouseover", function() {
+      if (window.innerWidth > 800) {
+          dropdownContent.style.display = "block";
       }
-      else {
-          dropbtn.innerText = "Возможности ▾";
-      }
-    }
-    
-    Resized();
   });
   
-  const MenuContainer = document.getElementsByClassName("menubar")[0];
-  const MenuOpener = document.getElementById("menu-opener");
-  const MenuLine = document.getElementsByClassName("menu-line");
-  let MenuOpened = false;
-  
-  if (MenuOpener && MenuContainer && MenuLine.length > 2) {
-    MenuOpener.addEventListener("click", function () {
-      if (!MenuOpened) {
-          MenuOpened = true;
-          MenuContainer.style.transform = "translate(0, 0)";
-          MenuLine[0].style.transform = "rotate(45deg)";
-          MenuLine[2].style.transform = "rotate(-45deg)";
-          MenuLine[0].style.transform = "translate(0, 24px)";
-          MenuLine[1].style.transform = "translate(0, -24px)";
-          MenuLine[1].style.opacity = "0";
-          MenuLine[1].style.transform = "translate(24px, 0)";
-      } else {
-          MenuOpened = false;
-          MenuContainer.style.transform = "translate(0, -100vh)";
-          MenuLine[0].style.transform = "rotate(0deg)";
-          MenuLine[2].style.transform = "rotate(0deg)";
-          MenuLine[0].style.transform = "translate(0, 0)";
-          MenuLine[1].style.transform = "translate(0, 0)";
-          MenuLine[1].style.opacity = "1";
-          MenuLine[1].style.transform = "translate(0, 0)";
+  dropdown.addEventListener("mouseout", function() {
+      if (window.innerWidth > 800) {
+          dropdownContent.style.display = "none";
       }
-    });
+  });
+});
+
+window.addEventListener("resize", function () {
+    Resized()
+})
+function Resized() {
+    if(window.innerWidth < 800) {
+        dropbtn.innerText = ""
+    }
+    else {
+        dropbtn.innerText = "Возможности ▾"
+    }
+}
+Resized()
+const MenuContainer = document.getElementsByClassName("menubar")[0]
+const MenuOpener = document.getElementById("menu-opener")
+const MenuLine = document.getElementsByClassName("menu-line")
+let MenuOpened = false
+MenuOpener.addEventListener("click", function () {
+  if (!MenuOpened) {
+      MenuOpened = true
+      MenuContainer.style.translate = "0 0"
+      MenuLine[0].style.rotate = "45deg"
+      MenuLine[2].style.rotate = "-45deg"
+      MenuLine[0].style.translate = "0 24px"
+      MenuLine[1].style.translate = "0 -24px"
+      MenuLine[1].style.opacity = "0"
+      MenuLine[1].style.translate = "24px 0"
   }
+  else {
+      MenuOpened = false
+      MenuContainer.style.translate = "0 -100vh"
+      MenuLine[0].style.rotate = "0deg"
+      MenuLine[2].style.rotate = "0deg"
+      MenuLine[0].style.translate = "0 0"
+      MenuLine[1].style.translate = "0 0"
+      MenuLine[1].style.opacity = "1"
+      MenuLine[1].style.translate = "0 0"
+  }
+})
